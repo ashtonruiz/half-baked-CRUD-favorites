564ca6002cd3e81f8a0ef0e67b030412fb9aeb28
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-28 13:20:46 -0800

    think I finally fixed it? I re-arranged stuff again in my todos.js and followed the live example code for it because I was just so lost and confused.

--- todos/todos.js
+++ todos/todos.js
@@ -18,46 +18,50 @@ const deleteButton = document.querySelector('.delete-button');
 todoForm.addEventListener('submit', async (e) => {
     // on submit, create a todo, reset the form, and display the todos
     e.preventDefault();
-    await createTodo();
-
     // create todo state
     const data = new FormData(todoForm);
     const todo = data.get('todo');
+    await createTodo(todo);
     todoForm.reset();
     // add async complete todo handler function
-    const newTodo = await completeTodo(todo);
+    // const newTodo = await completeTodo(todo);
     // call completeTodo
-    completeTodo();
+    // completeTodo();
     // swap out todo in array
     // call displayTodos
     displayTodos();
+});
 
-    deleteButton.addEventListener('click', async () => {
-        // delete all todos
-        await deleteAllTodos();
-        // modify state to match
-        await displayTodos();
-        // re displayTodos
-    });
-    async function displayTodos() {
-        // clear the container (.innerHTML = '')
-        const todos = await getTodos();
-        // display the list of todos,
-        todosEl.textContent = '';
-        // call render function, pass in state and complete handler function!
-        for (let todo of todos) {
-            const todoListEl = renderTodo(todo);
-            // add page load function
-            todoListEl.addEventListener('click', async () => {
-                // fetch the todos and store in state
-                await displayTodos(todo.id);
-                // call displayTodos
-                displayTodos();
-            });
-            // append to .todos
-            todosEl.append(todoListEl);
-        }
+async function displayTodos() {
+    // clear the container (.innerHTML = '')
+    const todos = await getTodos();
+    // display the list of todos,
+    todosEl.textContent = '';
+    // call render function, pass in state and complete handler function!
+    for (let todo of todos) {
+        const todoEl = renderTodo(todo);
+        // add page load function
+        todoEl.addEventListener('click', async () => {
+            // fetch the todos and store in state
+            await completeTodo(todo.id);
+            // call displayTodos
+            displayTodos();
+        });
+        // append to .todos
+        todosEl.append(todoEl);
     }
+}
+
+window.addEventListener('load', async () => {
+    displayTodos();
+});
+
+deleteButton.addEventListener('click', async () => {
+    // delete all todos
+    await deleteAllTodos();
+    // modify state to match
+    await displayTodos();
+    // re displayTodos
 });
 
 logoutButton.addEventListener('click', () => {
